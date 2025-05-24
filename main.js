befc628cea5cd0808e3231f262aa74d1824d8338
kcihaep <kcihaep@berkeley.edu>
2025-05-23 22:13:16 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// Store a reference to the <h1> in a variable
+const myHeading = document.querySelector("h1");
+// Update the text content of the <h1>
+myHeading.textContent = "Hello world!";
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+  
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
+  
+  myButton.addEventListener("click", () => {
+    setUserName();
+  });
+  
