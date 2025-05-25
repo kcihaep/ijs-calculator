91e68123f85be0d30605cca98ff62315d34bacde
Michael <kcihaep@berkeley.edu>
2025-05-24 23:01:09 -0700

    updating calculator.js

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,21 +1,21 @@
-function calculateScore(tableID) {
+function calculateScore() {
 
-    const table = document.getElementById(tableID);
-    const rows = table.rows;
-    const numCols = rows[0].cells.length;
+    var table = document.getElementById(free-skate);
+    var rows = table.rows;
+    var numCols = rows[0].cells.length;
   
-    const sumRow = table.insertRow(-1);
+    var sumRow = table.insertRow(-1);
   
     for (let i = 0; i < numCols; i++) {
       let sum = 0;
       for (let j = 1; j < rows.length; j++) {
-        const cellValue = parseFloat(rows[j].cells[i].textContent);
+        var cellValue = parseFloat(rows[j].cells[i].textContent);
         if (!isNaN(cellValue)) {
           sum += cellValue;
         }
       }
   
-      const sumCell = sumRow.insertCell(i)
+      var sumCell = sumRow.insertCell(i)
       sumCell.textContent = sum;
   
     }
