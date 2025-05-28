d443ff630d331341398005e1066bc9ae1c551435
Michael <kcihaep@berkeley.edu>
2025-05-28 10:12:46 -0700

    changed hotkeys and added hotkey for clearTable()

--- scripts/hotkeys.js
+++ scripts/hotkeys.js
@@ -1,16 +1,18 @@
    document.addEventListener('keydown', function(event) {
-     if (event.shiftKey && event.key === '+') {
-       console.log('+pressed!');
+     if (event.shiftKey && event.keyCode === 65) {
        addRow();
+       console.log("adding row");
      }
 
-     if (event.shiftKey && event.keyCode === 189) {
-       console.log('- pressed!');
+     if (event.shiftKey && event.keyCode === 68) {
        deleteRow();
      }
 
+    if (event.shiftKey && event.keyCode === 67) {
+       clearTable();
+     }
+
      if (event.shiftKey && event.key === "Enter") {
-      console.log("coaculate")
       calculateScore();
      }
    });
