665f028ddafd936feaaa4204ca1b8e6e4d275063
Michael <kcihaep@berkeley.edu>
2025-05-29 13:00:50 -0700

    hamburger menu

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,8 +1,9 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
   } else {
-    x.style.display = "block";
+    x.className = "topnav";
   }
 }
